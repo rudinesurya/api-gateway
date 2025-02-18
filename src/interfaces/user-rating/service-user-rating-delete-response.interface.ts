31f5ff8af38b9317bc18de9b580641be6d8e6df3
rudine <rudine.hartanto@intellisense.io>
2025-02-18 23:10:12 +0000

    user-ratings update and delete endpoints

---
+++ src/interfaces/user-rating/service-user-rating-delete-response.interface.ts
@@ -0,0 +1,5 @@
+export interface IServiceUserRatingDeleteResponse {
+    status: number;
+    message: string;
+    errors: { [key: string]: any };
+}
--- src/user-ratings.controller.ts
+++ src/user-ratings.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Inject, Param, Post, Req, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Inject, Param, Post, Put, Req, UseGuards } from "@nestjs/common";
 import { ClientProxy } from "@nestjs/microservices";
 import { ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiTags } from "@nestjs/swagger";
 import { firstValueFrom } from "rxjs";
@@ -9,6 +9,11 @@ import { CreateUserRatingDto } from "./interfaces/user-rating/dto/create-user-ra
 import { IServiceUserRatingCreateResponse } from "./interfaces/user-rating/service-user-rating-create-response.interface";
 import { JwtAuthGuard } from "./services/guards/jwt.guard";
 import { IAuthorizedRequest } from "./interfaces/common/authorized-request.interface";
+import { UpdateUserRatingResponseDto } from "./interfaces/user-rating/dto/update-user-rating-response.dto";
+import { UpdateUserRatingDto } from "./interfaces/user-rating/dto/update-user-rating.dto";
+import { IServiceUserRatingUpdateResponse } from "./interfaces/user-rating/service-user-rating-update-response.interface";
+import { DeleteUserRatingResponseDto } from "./interfaces/user-rating/dto/delete-user-rating-response.dto";
+import { IServiceUserRatingDeleteResponse } from "./interfaces/user-rating/service-user-rating-delete-response.interface";
 
 @Controller('user_ratings')
 @ApiTags('user_ratings')
@@ -47,9 +52,8 @@ export class UserRatingsController {
         @Req() request: IAuthorizedRequest,
         @Body() userRatingParams: CreateUserRatingDto,
     ): Promise<CreateUserRatingResponseDto> {
-        const userInfo = request.user;
         const createUserRatingResponse: IServiceUserRatingCreateResponse = await firstValueFrom(
-            this.userRatingsServiceClient.send('user_rating_create', { rater: userInfo.id, ...userRatingParams }),
+            this.userRatingsServiceClient.send('user_rating_create', { rater: request.user.id, ...userRatingParams }),
         );
         if (createUserRatingResponse.status !== HttpStatus.CREATED) {
             throw new HttpException(
@@ -67,6 +71,75 @@ export class UserRatingsController {
             data: {
                 user_rating: createUserRatingResponse.user_rating,
             },
+            errors: null,
+        };
+    }
+
+    @Put('/:ratingId')
+    @ApiBearerAuth('JWT-auth')
+    @UseGuards(JwtAuthGuard)
+    @ApiOkResponse({
+        type: UpdateUserRatingResponseDto,
+    })
+    public async updateUserRating(
+        @Param('ratingId') ratingId: string,
+        @Req() request: IAuthorizedRequest,
+        @Body() updateData: UpdateUserRatingDto,
+    ): Promise<UpdateUserRatingResponseDto> {
+        const updateUserRatingResponse: IServiceUserRatingUpdateResponse = await firstValueFrom(
+            this.userRatingsServiceClient.send('user_rating_update', { ratingId, raterId: request.user.id, updateData }),
+        );
+        if (updateUserRatingResponse.status !== HttpStatus.OK) {
+            throw new HttpException(
+                {
+                    message: updateUserRatingResponse.message,
+                    data: null,
+                    errors: updateUserRatingResponse.errors,
+                },
+                updateUserRatingResponse.status,
+            );
+        }
+
+        return {
+            message: updateUserRatingResponse.message,
+            data: {
+                user_rating: updateUserRatingResponse.user_rating,
+            },
+            errors: null,
+        };
+    }
+
+    @Delete('/:ratingId')
+    @ApiBearerAuth('JWT-auth')
+    @UseGuards(JwtAuthGuard)
+    @ApiOkResponse({
+        type: DeleteUserRatingResponseDto,
+    })
+    public async deleteUserRating(
+        @Param('ratingId') ratingId: string,
+        @Req() request: IAuthorizedRequest,
+    ): Promise<DeleteUserRatingResponseDto> {
+        const deleteUserRatingResponse: IServiceUserRatingDeleteResponse = await firstValueFrom(
+            this.userRatingsServiceClient.send('user_rating_delete_by_id', {
+                ratingId,
+                raterId: request.user.id,
+            }),
+        );
+
+        if (deleteUserRatingResponse.status !== HttpStatus.OK) {
+            throw new HttpException(
+                {
+                    message: deleteUserRatingResponse.message,
+                    errors: deleteUserRatingResponse.errors,
+                    data: null,
+                },
+                deleteUserRatingResponse.status,
+            );
+        }
+
+        return {
+            message: deleteUserRatingResponse.message,
+            data: null,
             errors: null,
         };
     }
